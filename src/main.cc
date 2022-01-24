f39841552571b012cda582f2b7ac57c9064a785d
yves <you@example.com>
2022-01-24 12:50:51 +0100

    [main] - hello vips

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,36 @@
+/* compile with:
+ *      g++ -g -Wall example.cc `pkg-config vips-cpp --cflags --libs`
+ */
+
+#include <cstdint>
+#include <iostream>
+
+#include <vips/vips8>
+#include <nlohmann/json.hpp>
+
+
+// for convenience
+using json = nlohmann::json;
+using namespace vips;
+using namespace std;
+
+constexpr bool debug = false;
+
+
 int main( int argc, char** argv) {
-    return 0;
+  if (VIPS_INIT (argv[0])) 
+    vips_error_exit (NULL);
+
+  if (argc < 1+1)
+    vips_error_exit ("usage: %s file* ", argv[0]);
+
+  int i = 1;
+  for( const char* name=argv[i++];i<=argc;name=argv[i++]) {
+      cerr << name << "\n";
+  }
+
+  vips_shutdown ();
+
+
+  return 0;
 }
