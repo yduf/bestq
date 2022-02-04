53d390c109d62d407e7bdc5df7b584f605389e7f
yves <you@example.com>
2022-02-04 10:31:28 +0100

    [noise] - measure

---
+++ src/README.md
@@ -0,0 +1,43 @@
+# [Find image with best quality in given list](https://yduf.github.io/image-quality/)
+
+Licensed under the [GNU General Public License, version 3. (GPLv3)](https://www.gnu.org/licenses/gpl-3.0.html)
+
+Based on following ideas to measure _image sharpness_ and _image noise_ of a set of images and compare their relatives quality.
+
+## [Image sharpness](https://photo.stackexchange.com/a/117823/104580)
+
+Image sharpness is evaluated by resizing all images to the bigger size & re-compress both images with exactly same quality preset, grayscale conversion, using JPEG or better algorithm.
+
+## [Image noise](https://stackoverflow.com/a/25436112/51386)
+
+Image noise is evaluated per image without resizing the image using a laplacian filter.
+More detail and improved approach can be found in [1](https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.173.1644&rep=rep1&type=pdf).
+
+- [\[1\]](https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.173.1644&rep=rep1&type=pdf)
+
+## Usage
+
+{% highlight bash %}
+$ ./bestq images/*
+{% endhighlight %}
+
+Output is a one line json string with the following format:
+
+{% highlight json %}
+[ // array of results
+    {
+        "file": "/path/to/image.jpg",     
+        "sharpness": 0.7683959817912913,  // 1.0 is max sharpness (normalized)
+        "jpg_size": 40005,                // used to evaluate absolute sharpness
+        "noise": 1.0,                     // 1.0 is min noise (normalized)
+        "sigma_noise": 0.5916566144368179 // absolute noise
+    },
+...
+]
+{% endhighlight %}
+
+where sharpness is relative to other images in the list, same for noise.
+
+This information gives qualitative metrics one can use to estimate the quality of images in the list,
+without relying only on the dimension of the images or their file size.
+
--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,8 @@
 
 #include <cstdint>
 #include <iostream>
+#include <map>
+//#include <ranges>
 
 #include <vips/vips8>
 #include <nlohmann/json.hpp>
@@ -24,10 +26,131 @@ int main( int argc, char** argv) {
   if (argc < 1+1)
     vips_error_exit ("usage: %s file* ", argv[0]);
 
+   int akernel[] = { 1,-2, 1,
+                    -2, 4,-2,
+                     1,-2, 1 };
+
+   VImage laplacian = VImage::new_from_memory( akernel, sizeof(akernel), 
+                                            3, 3, 1, VIPS_FORMAT_INT);
+
+  // 1 - gather max dimension from images
+  // we don't care about form factors
   int i = 1;
+  int max_width = 0;
+  int max_height = 0;
+
+  map<string, double> noise;
+  double min_sigma = INFINITY;
+
   for( const char* name=argv[i++];i<=argc;name=argv[i++]) {
-      cerr << name << "\n";
+    cerr << name << "\n";
+    VImage in = VImage::new_from_file (name, VImage::option ()->set ("access", VIPS_ACCESS_SEQUENTIAL) );
+    
+    if( debug)
+      cerr << "  " << in.width() << "x" << in.height() << "\n";
+
+    if (in.width() > max_width)
+      max_width = in.width();
+
+    if( in.height() > max_height)
+      max_height = in.height();
+
+    // estimate noise
+    VImage conv = in.colourspace(VIPS_INTERPRETATION_sRGB, 
+                                 VImage::option()
+                                    ->set ("source_space", VIPS_INTERPRETATION_B_W)
+                                  )
+                      .conv( laplacian )
+                      .abs()
+                ;
+
+    if( debug)
+      conv.write_to_file("conv.pgm");
+    
+    VImage sum_rows; // = VImage::VImage::new_memory();
+    VImage sum_columns = conv.project( &sum_rows) / ( 6.0 * (in.height() -2) );
+    // cerr << "sum_rows  " << sum_rows.width() << "x" << sum_rows.height() << "\n";
+    // cerr << "sum_columns  " << sum_columns.width() << "x" << sum_columns.height() << "\n";
+             
+    VImage sum = VImage::VImage::new_memory();
+    sum_columns.project( &sum );
+    // cerr << "sum  " << sum.width() << "x" << sum.height() << "\n";
+
+    auto* p = (double*) sum[0].data();
+    double sigma = sqrt( 0.5 * M_PI) / ( conv.width()-2)* *p;
+    // cerr << "sigma  " << sigma << "\n";
+
+    noise[name] = sigma;
+
+    if (sigma < min_sigma)
+      min_sigma = sigma;
+
+    /*
+    auto sigma = sqrt( 0.5 * M_PI) / (6 * ( in.width()-2) * (in.height()-2))* *p;
+    cerr << "  sigma: " << sigma << "\n";
+
+    */
   }
+
+  if( debug) {
+    cerr << "max_width: " << max_width << "\n";
+    cerr << "max_height: " << max_height << "\n";
+  }
+
+  // 2 - rescale all images to max dimension
+  // 3 - save them in jpg with same parameters
+  // and use the size as an indicator of the sharpness.
+  std::multimap<size_t, string> sharpness_map;
+
+  i = 1;
+  for( const char* name=argv[i++];i<=argc;name=argv[i++]) {
+    if( debug)
+      cerr << name << "\n";
+
+    void *buf;
+    size_t size;
+
+      VImage rescaled = VImage::thumbnail( name, max_width,
+                                      VImage::option()
+                                        ->set( "size", VIPS_SIZE_FORCE)
+                                        ->set( "height", max_height)
+                                        ->set( "no_rotate", true)
+                                    )
+                        .colourspace(VIPS_INTERPRETATION_sRGB, 
+                                 VImage::option()
+                                    ->set ("source_space", VIPS_INTERPRETATION_B_W)
+                                  )
+                        ;          
+        rescaled.write_to_buffer( ".jpg", &buf, &size );
+        // cerr << size << "\n";
+
+        //sharpness_map[size] = name;
+        sharpness_map.insert(  std::multimap<size_t, string>::value_type( size, name));
+
+        free( buf);
+  }
+
+  // output json
+      // JSON output
+    json j;
+    int max_sharpness = sharpness_map.rbegin()->first;
+
+    for( const auto& [size, file] : sharpness_map) {
+        /*j["sharpness"].push_back({
+            {"size", k},
+            {"file", v}
+        });*/
+        json e = {  { "file",  file},
+                    { "sharpness", (double) size / max_sharpness},  // 1.0 is max sharpness (normalized)
+                    { "jpg_size", size},                            // used to evaluate absolute sharpness
+                    { "noise", noise[file] / min_sigma },           // 1.0 is min noise (normalized)
+                    { "sigma_noise", noise[file] }                  // absolute noise
+                };
+        j.push_back( e);
+//        cerr << k << " " << v << "\n" << endl;
+    }
+
+    std::cout << j << "\n";
 
   vips_shutdown ();
 
